130c7365ac6838d434984008d094b414ecdabb14
AndreySemeynikovIF <andrey.semeynikov@ifortex.com>
2025-04-07 11:32:23 +0300

    MED-54: Get name while updating geo (#16)

--- accounting-api/src/main/java/com/ifortex/internship/accountingapi/AccountingServiceApi.java
+++ accounting-api/src/main/java/com/ifortex/internship/accountingapi/AccountingServiceApi.java
@@ -12,20 +12,32 @@ import java.util.UUID;
 
 @FeignClient(
     name = "accounting-service",
-    path = "/api/v1/accounting",
+    path = "/api/v1",
     configuration = FeignClientConfiguration.class)
 public interface AccountingServiceApi {
 
     /**
-     * Retrieves a user profile by their accountId from the auth-service.
+     * Retrieves a user profile by their accountId from the accounting-service.
      *
-     * <p>This method calls the auth-service endpoint to fetch a user's data by userId and returns it
+     * <p>This method calls the accounting-service endpoint to fetch a user's data by userId and returns it
      * as a {@link AccountDto}.
      * @param accountId the accountId of the user to retrieve.
      * @return a {@code ResponseEntity} containing the {@code AccountDto} with user details, or a {@code ResponseEntity} with an appropriate HTTP
      * status if the user is not found.
      * @throws FeignException if there is an issue with the communication with the auth-service.
      */
-    @GetMapping("/{accountId}")
+    @GetMapping("/accounting/{accountId}")
     ResponseEntity<?> getUserProfileById(@PathVariable("accountId") UUID accountId);
+
+    /**
+     * Retrieves a user profile by their authentication from the accounting-service.
+     *
+     * <p>This method calls the accounting-service endpoint to fetch a user's data by authentication and returns it
+     * as a {@link AccountDto}.
+     * @return a {@code ResponseEntity} containing the {@code AccountDto} with user details, or a {@code ResponseEntity} with an appropriate HTTP
+     * status if the user is not found.
+     * @throws FeignException if there is an issue with the communication with the auth-service.
+     */
+    @GetMapping("/account")
+    ResponseEntity<?> getUserProfileByAuthentication();
 }
